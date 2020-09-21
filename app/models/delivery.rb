1e4d827636d0edc16f1b99ead1c7c22f64631a5d
石川　将理 <masamichi@ishikawasusumurinoMacBook-Air.local>
2020-09-21 12:02:36 +0900

    first commit

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,7 @@
 class Delivery < ApplicationRecord
+	belongs_to :customer
+
+	validates :postal_code
+	validates :name, presence: true, length: {maximum: 10, minimum: 2}, uniqueness: true
+	validates :address, presence: true
 end
