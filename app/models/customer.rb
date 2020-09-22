aec27ae657d5e663bfc76cad58e0913c52021d40
vagrant <vagrant@localhost.localdomain>
2020-09-22 07:56:45 +0000

    [create]sign_up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,24 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :last_name, presence: true
+validates :first_name, presence: true
+validates :last_name_kana, presence: true,
+                 format: {
+                   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                   message: "全角カタカナのみで入力して下さい"
+                 }
+validates :first_name_kana, presence: true,
+                 format: {
+                   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                   message: "全角カタカナのみで入力して下さい"
+                 }
+
+validates :postal_code, numericality: { only_integer: true }
+# 郵便番号を数値だけ入力可,
+
+validates :address, presence: true
+validates :telephone_number, numericality: { only_integer: true }
+# 電話番号を数値だけ入力可、
 end
