28a009f01c65ef98ef024f5c696de1f0cfe0953a
vagrant <vagrant@localhost.localdomain>
2020-09-23 09:28:31 +0000

    first-commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
 	has_many :cart_items, dependent: :destroy
 	has_many :order_items, dependent: :destroy
-	belongs_to :genres, dependent: :destroy
+	belongs_to :genre, dependent: :destroy
 	attachment :image
+	validates :name, presence: true
+	validates :introduction, presence: true
+	validates :price, presence: true
+	validates :is_active, presence: true
+
 end
