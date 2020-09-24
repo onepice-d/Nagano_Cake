fb97d3d02d059695bc5832d3d74ef807791f4bf0
vagrant <vagrant@localhost.localdomain>
2020-09-24 02:24:08 +0000

    kimoto

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,8 +6,12 @@ class Admin::GenresController < ApplicationController
 
 	def create
 		@genre = Genre.new(genre_params)
-		@genre.save
-    	redirect_to admin_genres_path
+		if @genre.save
+    		redirect_to admin_genres_path
+    	else
+		    @genres = Genre.all
+		    render :index
+		end
 	end
 
 	def edit
@@ -16,11 +20,16 @@ class Admin::GenresController < ApplicationController
 
 	def update
 		@genre = Genre.find(params[:id])
-		@Genre.update(genre_params)
+		if @genre.update(genre_params)
+			redirect_to admin_genres_path
+		else
+			render :edit
+		end
+
 	end
 
 	private
 	def genre_params
-		params.require(:genre).permit(:name, :status)
+		params.require(:genre).permit(:name, :is_active)
 	end
 end
