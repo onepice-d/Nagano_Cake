f52ac94b68e298532e10b5203d67a4ce84d35e20
vagrant <vagrant@localhost.localdomain>
2020-09-28 16:59:52 +0000

    kimoto

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,5 @@ class Order < ApplicationRecord
 	enum payment_method: {"銀行振込":0, "クレジットカード":1}
 
 
-	enum status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送中":4}
+	enum status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4}
    end
