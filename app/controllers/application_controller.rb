bf44ff162a51a4b5751c5a64e51517b7a4fe5246
石川　将理 <masamichi@ishikawasusumurinoMacBook-Air.local>
2020-09-29 01:49:13 +0900

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,16 @@ class ApplicationController < ActionController::Base
     			admin_top_path
     		when Customer
     			public_items_path
+		end
 	end
-end
+	def after_sign_out_path_for(resource)
+      if resource == :admin
+      new_admin_session_path
+      else
+      new_customer_session_path
+      end
+    end
+
+
+
 end
