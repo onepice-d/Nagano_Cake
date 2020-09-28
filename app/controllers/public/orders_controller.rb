ae91fc960008d7c19d8840ea3f328cde14eb9ed4
vagrant <vagrant@localhost.localdomain>
2020-09-28 05:31:30 +0000

    kimoto

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,10 +11,10 @@ class Public::OrdersController < ApplicationController
       	@order = Order.new(order_params)
       	@order.customer_id = current_customer.id
       	# 住所のラジオボタン選択に応じて引数を調整
-      	 @add = params[:order][:add].to_i
+      	 @add = params[:order][:address].to_i
       	 case @add
         when 1
-          @order.post_code = @customer.post_code
+          @order.postal_code = @customer.postal_code
           @order.address = @customer.address
           @order.name = full_name(@customer)
         when 2
@@ -31,7 +31,7 @@ class Public::OrdersController < ApplicationController
       # send_to_addressで住所モデル検索、該当データなければ新規作成
       if Delivery.find_by(address: @address).nil?
         @delivery = Delivery.new
-        @delivery.post_code = @order.postal_code
+        @delivery.postal_code = @order.postal_code
         @delivery.address = @order.address
         @delivery.name = @order.name
         @delivery.customer_id = current_customer.id
@@ -43,7 +43,7 @@ class Public::OrdersController < ApplicationController
         order_item = @order.order_items.build
         order_item.order_id = @order.id
         order_item.item_id = cart_item.item_id
-        order_item.amount = cart_item.quantity
+        order_item.amount = cart_item.amount
         order_item.price = cart_item.item.price
         order_item.save
         cart_item.destroy #order_itemに情報を移したらcart_itemは消去
@@ -51,17 +51,32 @@ class Public::OrdersController < ApplicationController
       render :thanks
     else
       redirect_to public_items_path
-　　　flash[:danger] = 'カートが空です。'
     end
   end
 
 
 	def confirm
     params[:order][:payment_method] = params[:order][:payment_method].to_i
-    params[:order][:selected_address] = params[:order][:selected_address].to_i
     @order = Order.new(order_params)
-
-
+    @customer = current_customer
+    @cart_items = current_customer.cart_items
+    @add = params[:order][:add].to_i
+    case @add
+      when 1
+        @order.postal_code = @customer.postal_code
+        @order.address = @customer.address
+        @order.name = @customer.last_name + @customer.first_name
+      when 2
+        @address = params[:order][:delivery]
+        @delivery = Delivery.find(@address)
+        @order.postal_code = @delivery.postal_code
+        @order.address = @delivery.address
+        @order.name = @delivery.name
+      when 3
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
+    end
   end
 
 
@@ -88,7 +103,8 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(
-      :created_at, :postal_code, :address, :status, :payment_method, :postal_code, :shipping_cost, :name, :selected_address,
+      :created_at,
+      :address, :status, :payment_method, :postal_code, :shipping_cost, :name, :selected_address,
       order_items_attributes: [:order_id, :item_id, :quantity, :order_price, :make_status]
       )
   end
